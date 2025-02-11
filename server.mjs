904ae68a74c2719ab4cb5c45e2ba0b068b6a697b
Involentary-Pterodacyl <153659214+Involentary-Pterodacyl@users.noreply.github.com>
2025-02-11 13:31:35 -0500

    2/11/25 end of class

--- server.mjs
+++ server.mjs
@@ -3,6 +3,8 @@ import mysql from "mysql2";
 import cors from "cors";
 import res from "express/lib/response.js";
 import http from "http";
+import { Buffer } from 'node:buffer';
+// const express = require('express');
 const app = express();
 
 
@@ -42,16 +44,12 @@ app.listen(port, () => {
 });
 
 function getData() {
-    const postData = JSON. stringify({
-        'msg': 'Hello World!',
-    });
+    //let str = '';
 
-    let str = '';
-
-    const options = {
-        host: 'http://localhost:3000',
-        path: '/'
-    };
+    // const options = {
+    //     host: 'http://localhost:3000',
+    //     path: '/'
+    // };
 
     // let callback = function (response) {
     //
@@ -69,23 +67,38 @@ function getData() {
     // }
     //let req = http.request(options, callback).end();
 
-    const req = http. request(options, (res) => {
-        console. log(`STATUS: ${res. statusCode}`);
-        console. log(`HEADERS: ${JSON. stringify(res. headers)}`);
-        res. setEncoding('utf8');
-        res. on('data', (chunk) => {
-            console. log(`BODY: ${chunk}`);
+    const postData = JSON.stringify({
+        'msg': 'Hello World!',
+    });
+
+    const options = {
+        hostname: 'www.google.com',
+        port: port,
+        path: '/upload',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData),
+        },
+    };
+
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
         });
-        res. on('end', () => {
-            console. log('No more data in response.');
+        res.on('end', () => {
+            console.log('No more data in response.');
         });
     });
 
-    req. on('error', (e) => {
-        console. error(`problem with request: ${e. message}`);
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
     });
 
 // Write data to request body
-    req. write(postData);
-    req. end();
+    req.write(postData);
+    req.end();
 }
