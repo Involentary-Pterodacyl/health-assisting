f716d90143f2352daa8170ab74b82642ef0f8369
Involentary-Pterodacyl <153659214+Involentary-Pterodacyl@users.noreply.github.com>
2025-02-27 13:43:20 -0500

    working on being able to select buttons on bed mobility page.

---
+++ front end/server connection js files/bed mobility.js
@@ -0,0 +1,8 @@
+let total = document.getElementById('total');
+let extensive = document.getElementById('extensive');
+let limited = document.getElementById('limited');
+let supervision = document.getElementById('supervision');
+let independent = document.getElementById('independent');
+let none = document.getElementById('none');
+
+let twoPerson = document.getElementById('two person');
