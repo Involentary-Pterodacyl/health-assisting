f0dc80d342a22e9c0e08a24d3da0584a37b2f4e5
Involentary-Pterodacyl <153659214+Involentary-Pterodacyl@users.noreply.github.com>
2025-03-14 13:34:29 -0400

    dietary intake path sent to server compleate

--- front end/server connection js files/login.js
+++ front end/server connection js files/login.js
@@ -24,7 +24,7 @@ login.onclick = function (){
 };
 
 function sendData(tableName, value, patientNum) {
-  axios.post('http://localhost:3000/submit', {tableName: tableName, userId: id, patientId: patientNum, value: value})
+  axios.post('http://localhost:3000/submit', {tableName: tableName, userId: id,  value: value, patientId: patientNum})
     .then(response => {
       console.log('Response:', response.data);
     })
@@ -32,4 +32,25 @@ function sendData(tableName, value, patientNum) {
       console.error('Error:', error);
     });
 }
-export {sendData};
+
+function sendMealData(valueB, valueL, valueD, patientNum) {
+  axios.post('http://localhost:3000/submitMeal', {tableName: "dietary_intake_meal", userId: id,  breakfast: valueB, lunch: valueL, dinner: valueD, patientId: patientNum})
+    .then(response => {
+      console.log('Response:', response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function sendInOutData(tableName, valueD, valueI, patientNum) {
+  axios.post('http://localhost:3000/submitInOut', {tableName: tableName, userId: id,  ccIn: valueD, ccOut: valueI, patientId: patientNum})
+    .then(response => {
+      console.log('Response:', response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+export {sendData, sendMealData, sendInOutData};
