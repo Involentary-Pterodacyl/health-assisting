55bab4978f5de33a9c4d502b5b6faafa72881c35
Involentary-Pterodacyl <153659214+Involentary-Pterodacyl@users.noreply.github.com>
2025-04-01 12:22:55 -0400

    working username!

--- server.mjs
+++ server.mjs
@@ -33,15 +33,18 @@ let isUser;
 //getting data
 app.post('/login', (req, res) => {
 
-    const conn = mariadb.createConnection({host: '127.0.0.1', user:'db_user', password: 'HA-db', database: 'health_assisting'});
-    conn.query("SELECT username FROM users where username='Tlllll'", (err, rows) => { //as val ????   // works add chageabliliy from user to user
-        console.log(rows); //[ {val: 1}, meta: ... ]
-            conn.end();
+    const conn = mariadb.createConnection({host: '127.0.0.1', user: 'db_user', password: 'HA-db', database: 'health_assisting'});
+    conn.query("SELECT username FROM users where username='" + req.body.username + "'", (err, rows) => {
+        console.log(rows);
+        if (rows.length > 0) {
+            console.log(rows[0]["username"]); //the actual value as a string
+        }
+        else {
+            console.log("Invalid username.");
+        }
+        conn.end();
     })
 });
-
-
-
 
 // Handle POST requests
 app.post('/submit', (req, res) => {
